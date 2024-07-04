84dfbb9c77bdd662477112eb87bc6653c9718244
silber175 <y.silber175@gmail.com>
2024-07-05 01:33:50 +0300

    Task1 commit 1

---
+++ src/main/java/Task3/Execise3.java
@@ -0,0 +1,15 @@
+package Task3;
+
+import java.util.LinkedList;
+
+public class Execise3 {
+    public static void main(String[] args) {
+        LinkedList<Runnable> taskList = new LinkedList<>();
+        for (int ii = 1; ii < 9; ii++) {
+            for (int jj = 1; jj < 9; jj++) {
+                taskList.add(new Task((ii * jj), ii, jj));
+            }
+        }
+        new TreadPoolExecutor(32, taskList);
+    }
+}
